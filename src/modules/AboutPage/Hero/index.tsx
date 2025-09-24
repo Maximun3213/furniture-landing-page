f7fb64ef04e05eef86af11a3377e6d514e11af49
max <max>
2025-09-24 20:05:49 +0700

    Refactor AboutPage component to replace image placeholders with a Hero component for improved content presentation and structure.

---
+++ src/modules/AboutPage/Hero/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import FadeText from "@/animation/components/FadeText";
+import LineMaskText from "@/animation/components/LineMaskText";
+import ArrowEffect from "@/components/ArrowEffect";
+import { TypoColor, TypoTagHeading, TypoTagParagraph } from "@/enums/typo";
+import { Box, Flex } from "@chakra-ui/react";
+import ImagePlaceHolder from "@Components/ImagePlaceHolder";
+import Heading from "@Components/Typo/Heading";
+import Paragraph from "@Components/Typo/Paragraph";
+import React from "react";
+
+function Hero(): React.JSX.Element {
+  return (
+    <Box className={"home"} position={"relative"}>
+      <Box>
+        <ImagePlaceHolder
+          src={"/img/hero_about.jpg"}
+          alt={"unsplash"}
+          width={1920}
+          height={1080}
+        />
+      </Box>
+      <Flex
+        flexDirection={"column"}
+        gap={"5rem"}
+        position={"absolute"}
+        top={"50%"}
+        transform={"translateY(-50%)"}
+        right={"16.2rem"}
+        bgColor={"var(--black)"}
+        padding={"12.5rem"}
+      >
+        <FadeText>
+          <Heading
+            size={48}
+            fontWeight={400}
+            as={TypoTagHeading.h1}
+            color={TypoColor.white_primary}
+          >
+            About Us
+          </Heading>
+        </FadeText>
+        <LineMaskText>
+          <Paragraph
+            size={18}
+            fontWeight={500}
+            width={"52.9rem"}
+            as={TypoTagParagraph.p}
+            color={TypoColor.white_primary}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            condimentum adipiscing sapien proin. Aliquet amet non enim, ut
+            aliquet in pulvinar eu. er condimentum adipiscing sapien proin.
+            Aliquet amet entum adipiscing sapien proin.
+          </Paragraph>
+        </LineMaskText>
+        <ArrowEffect />
+      </Flex>
+    </Box>
+  );
+}
+
+export default Hero;
--- src/modules/AboutPage/index.tsx
+++ src/modules/AboutPage/index.tsx
@@ -1,41 +1,11 @@
-import ImagePlaceHolder from "@/components/ImagePlaceHolder";
-import { Grid } from "@chakra-ui/react";
 import React from "react";
+import Hero from "./Hero";
 
 const AboutPage = (): React.JSX.Element => {
   return (
-    <Grid gridTemplateColumns={"repeat(3, 1fr)"} gap={4} marginTop={"5rem"}>
-      <div>
-        <ImagePlaceHolder
-          src={
-            "https://images.unsplash.com/photo-1726064855757-ac8720008fe0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          alt={"unsplash"}
-          width={500}
-          height={500}
-        />
-      </div>
-      <div>
-        <ImagePlaceHolder
-          src={
-            "https://images.unsplash.com/photo-1533563906091-fdfdffc3e3c4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          alt={"unsplash"}
-          width={500}
-          height={500}
-        />
-      </div>
-      <div>
-        <ImagePlaceHolder
-          src={
-            "https://images.unsplash.com/photo-1646444254678-b937f153b221?q=80&w=3840&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          alt={"unsplash"}
-          width={500}
-          height={500}
-        />
-      </div>
-    </Grid>
+    <>
+      <Hero />
+    </>
   );
 };
 
